d8f14057b08c2746bbf9aae53a74b3651bbd476f
Diego Romel <romeljogo@gmail.com>
2024-07-20 20:34:40 -0400

    atualiza a `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,4 @@
-<<<<<<<<< Temporary merge branch 1
-import { homedir } from "os";
-
 function Home() {
-  return <h1>Aqui o seu projeto é visto pelo mundo!</h1>;
-}
-
-export default homedir;
-=========
-function Home() {
-  return <d1>Aqui o seu projeto é visto pelo mundo!</d1>;
+  return <h1>Aqui o mundo enxerga o seu projeto!</h1>;
 }
 export default Home;
->>>>>>>>> Temporary merge branch 2
